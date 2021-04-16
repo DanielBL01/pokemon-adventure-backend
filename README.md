102c9f5ab362d4b7fb38caf43562d0ac58c8a038
DanielBL01 <dblee0532@gmail.com>
2021-04-16 00:45:30 -0400

    calling rest api

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pokemon Adventure
+
+Use the PokeAPI to create a pokemon simulator with a Pokedex to learn all about Pokemon!
+Since there are so many Pokemon, this project will only use the first 151 Pokemon from the Kanto Region (First Generation)
--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const axios = require('axios');
 
 app.get('/', async (req, res) => {
     try {
-        const result = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-        res.send(result.data);
+        const result = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+        res.send(result.data); // Send JSON data response
     }
     catch (err) {
+        console.error(err);
+    }
+});
+
+app.get('/pokemon', async (req, res) => {
+    var pokemon_name = req.query.name;
+    try {
+        const result = await axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemon_name);
+        res.send(result.data);
+    } catch (err) {
         console.error(err);
     }
 });
